90e3b164e8ba248755b68969cbb64b2b1c4b1310
jessica kilby <jessicakilby@gmail.com>
2016-09-07 09:41:23 -0500

    minor changes

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TSA Random Arrow
+#Javascript Exercise: TSA Random Arrow
 
 ## Description
 Your job is to create a web page that, when the user clicks anywhere in the window, randomly determine if an air traveller should go down the left lane or the right lane by displaying an arrow in that direction.
--- random.js
+++ random.js
@@ -1,1 +1,8 @@
 // Your job is to create a web page that, when the user clicks anywhere in the window, randomly determine if an air traveller should go down the left lane or the right lane by displaying an arrow in that direction.
+
+function getRandomNumber(upper) {
+  var randomNumber = Math.floor( Math.random() * upper ) + 1; 
+  return randomNumber;
+}
+console.log(getRandomNumber(6));
+console.log(getRandomNumber(1000));
