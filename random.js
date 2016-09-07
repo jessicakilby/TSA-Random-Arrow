b6c98c63300a74aeb7914d5ccf91634e2e7fd1ae
jessica kilby <jessicakilby@gmail.com>
2016-09-07 17:49:21 -0500

    done :)

--- random.js
+++ random.js
@@ -1,8 +1,14 @@
 // Your job is to create a web page that, when the user clicks anywhere in the window, randomly determine if an air traveller should go down the left lane or the right lane by displaying an arrow in that direction.
+var output = document.getElementById("arrow");
+var changeArrow = document.getElementById("submit");
 
-function getRandomNumber(upper) {
-  var randomNumber = Math.floor( Math.random() * upper ) + 1; 
-  return randomNumber;
+function getRandomNumber() {
+  var randomNumber = Math.floor( Math.random() * 10 ) + 1; 
+  if (randomNumber <= 5){
+  	output.innerHTML = `<div><img src="http://www.wpclipart.com/phpimageeditor/editimagesworkwith/Arrow_comic_left_red_1473286564_50_207_137_70.png"></div>`;
+  } else {
+  	output.innerHTML = `<div><img src="http://www.wpclipart.com/phpimageeditor/editimagesworkwith/Arrow_comic_right_green_1473287016_50_207_137_70.png"></div>`;
+  }
 }
-console.log(getRandomNumber(6));
-console.log(getRandomNumber(1000));
+changeArrow.addEventListener("click", getRandomNumber);
+
