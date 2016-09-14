45bad96ab03e68eb4d812de6ba5e0a61614c7889
jessica kilby <jessicakilby@gmail.com>
2016-09-14 10:05:44 -0500

    got rid of button, can click img instead

--- random.js
+++ random.js
@@ -1,14 +1,14 @@
 // Your job is to create a web page that, when the user clicks anywhere in the window, randomly determine if an air traveller should go down the left lane or the right lane by displaying an arrow in that direction.
 var output = document.getElementById("arrow");
-var changeArrow = document.getElementById("submit");
+// var changeArrow = document.getElementById("submit");
 
 function getRandomNumber() {
   var randomNumber = Math.floor( Math.random() * 10 ) + 1; 
   if (randomNumber <= 5){
-  	output.innerHTML = `<div><img src="http://www.wpclipart.com/phpimageeditor/editimagesworkwith/Arrow_comic_left_red_1473286564_50_207_137_70.png"></div>`;
+  	output.innerHTML = `<div><img src="arrow-left.png"></div>`;
   } else {
-  	output.innerHTML = `<div><img src="http://www.wpclipart.com/phpimageeditor/editimagesworkwith/Arrow_comic_right_green_1473287016_50_207_137_70.png"></div>`;
+  	output.innerHTML = `<div><img src="right_arrow.gif"></div>`;
   }
 }
-changeArrow.addEventListener("click", getRandomNumber);
+output.addEventListener("click", getRandomNumber);
 
